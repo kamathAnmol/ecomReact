ecd8214098a1508bd9827fd9e5168cf6d10cc5bd
kamathAnmol <anmol.k@pacewisdom.com>
2023-08-17 14:10:37 +0530

    error fixing

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { persistor, store } from "./store/store";
-import { PersistGate } from "redux-persist/integration/react";
+// import { persistor, store } from "./store/store";
+// import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
